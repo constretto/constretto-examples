6526658bff82551697357e47174702983ce4eff5
Kaare Nilsen <kaare.nilsen@arktekk.no>
2012-05-04 16:08:30 +0200

    o and the tag

--- dashboard/src/main/java/org/constretto/web/tag/ConstrettoForEachTag.java
+++ dashboard/src/main/java/org/constretto/web/tag/ConstrettoForEachTag.java
@@ -1,11 +1,44 @@
 package org.constretto.web.tag;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kaanilse
- * Date: 04.05.12
- * Time: 15:19
- * To change this template use File | Settings | File Templates.
- */
-public class ConstrettoForEachTag {
+import org.constretto.ConstrettoConfiguration;
+import org.constretto.Property;
+import org.constretto.exception.ConstrettoException;
+
+import javax.servlet.jsp.JspTagException;
+import java.util.Iterator;
+
+public class ConstrettoForEachTag extends javax.servlet.jsp.jstl.core.LoopTagSupport {
+    private static ConstrettoConfiguration configuration;
+    private Iterator<String> iterator = null;
+    private String expression;
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    protected Object next() throws JspTagException {
+        return iterator.next();
+    }
+
+    @Override
+    protected boolean hasNext() throws JspTagException {
+        return iterator.hasNext();
+    }
+
+    @Override
+    protected void prepare() throws JspTagException {
+        iterator = configuration.evaluateToList(String.class,expression).iterator();
+    }
+
+    private static ConstrettoConfiguration configuration() {
+        if (configuration == null) {
+            throw new ConstrettoException("Missing consretto config, did you remember to register the ConstrettoTaglibListener?");
+        }
+        return configuration;
+    }
+
+    public static void configuration(ConstrettoConfiguration configuration) {
+        ConstrettoForEachTag.configuration = configuration;
+    }
 }
